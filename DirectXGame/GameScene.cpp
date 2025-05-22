04e405cb5428cb376341696aa6de6e1bcbc72177
RyuJyashinn <k024g1044@g.neec.ac.jp>
2025-05-22 14:41:07 +0800

    2.4ok

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -4,46 +4,40 @@
 
 using namespace KamataEngine;
 void GameScene::Initialize() {
-	// 初始化基础组件
 	worldTransform_.Initialize();
 	camera_.Initialize();
 
-	// 初始化模型资源
 	modelBlock_ = Model::CreateFromOBJ("block");
 	modelPlayer_ = Model::CreateFromOBJ("player");
 
-	// 初始化地图数据（必须先于方块初始化）
 	mapChipField_ = new MapChipField;
 	mapChipField_->LoadMapChipCsv("Resources/blocks.csv");
 
-	// 初始化方块世界变换
 	const uint32_t kNumBlockHorizontal = mapChipField_->GetNumBlockHorizontal();
 	const uint32_t kNumBlockVirtical = mapChipField_->GetNumBlockVirual();
 	worldTransformBlocks_.resize(kNumBlockVirtical);
 	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
-		worldTransformBlocks_[i].resize(kNumBlockHorizontal, nullptr);
+		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
 	}
 
 	for (uint32_t y = 0; y < kNumBlockVirtical; ++y) {
-		for (uint32_t x = y % 2; x < kNumBlockHorizontal; ++x) {
-			if (mapChipField_->GetMapChipTypeByIndex(y, x) == MapChipType::kBlock) {
+		for (uint32_t x = 0; x < kNumBlockHorizontal; ++x) {
+			if (mapChipField_->GetMapChipTypeByIndex(x, y) == MapChipType::kBlock) {
 				WorldTransform* worldTransform = new WorldTransform();
 				worldTransform->Initialize();
 				worldTransformBlocks_[y][x] = worldTransform;
-				worldTransformBlocks_[y][x]->translation_ = mapChipField_->GetMapChipPositionByIndex(y, x);
+				worldTransformBlocks_[y][x]->translation_ = mapChipField_->GetMapChipPositionByIndex(x, y);
 			}
 		}
 	}
 
-	// 初始化调试相机
 	debugCamera_ = new DebugCamera(1280, 720);
 	PrimitiveDrawer::GetInstance()->SetCamera(&camera_);
 
-	// 初始化天空盒
 	skydome_ = new SkyDome();
 	skydome_->Initialize();
 
-	// 初始化玩家
+
 	player_ = new Player();
 	player_->Initialize(modelPlayer_, &camera_);
 }
